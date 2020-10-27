cc022a7e7ccc2eb4819ad117af4c70ebba635dc5
Jonathan Anthony <jonataanthony@gmail.com>
2020-10-26 22:09:44 -0300

    First commit

---
+++ src/main/java/com/gofurther/furtherBank/FurtherBankApplication.java
@@ -0,0 +1,13 @@
+package com.gofurther.furtherBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurtherBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurtherBankApplication.class, args);
+	}
+
+}
